b813e535f5f6440f065e9d628a291c7e42e88b06
Macbook <macbook@Macbooks-MacBook-Pro.local>
2026-01-21 06:13:55 +0300

    Add Chili game implementation files

--- ChiliImpl.cpp
+++ ChiliImpl.cpp
@@ -81,5 +81,17 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
-    gfx.PutPixel( 100, 100, 255, 255, 255 );
+    gfx.PutPixel( 395, 300, 255, 255, 255 );
+    gfx.PutPixel( 396, 300, 255, 255, 255 );
+    gfx.PutPixel( 397, 300, 255, 255, 255 );
+    gfx.PutPixel( 403, 300, 255, 255, 255 );
+    gfx.PutPixel( 404, 300, 255, 255, 255 );
+    gfx.PutPixel( 405, 300, 255, 255, 255 );
+    gfx.PutPixel( 400, 295, 255, 255, 255 );
+    gfx.PutPixel( 400, 296, 255, 255, 255 );
+    gfx.PutPixel( 400, 297, 255, 255, 255 );
+    gfx.PutPixel( 400, 303, 255, 255, 255 );
+    gfx.PutPixel( 400, 304, 255, 255, 255 );
+    gfx.PutPixel( 400, 305, 255, 255, 255 );
+    
 }
